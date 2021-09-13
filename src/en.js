e12abe3409281c9949bc5312ce601228928a7ea9
David Aerne <meodai@gmail.com>
2021-09-13 13:51:35 +0300

    feat: 🎸 Makes it possible to load other color descriptions
    The class now takes a second argument on init, what can provide
    alternative descriptsion, either simplified ones or in an other language

---
+++ src/en.js
@@ -0,0 +1,1012 @@
+export default {
+  "descriptions": [{
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.75,
+            1
+          ],
+          [
+            0.4,
+            0.55
+          ]
+        ]
+      },
+      "descriptive": [
+        "saturated",
+        "strong",
+        "lush",
+        "ablaze",
+        "beaming",
+        "bold",
+        "brilliant",
+        "flamboyant",
+        "vibrant",
+        "vivid",
+        "loud"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          [
+            0,
+            0.07
+          ]
+        ]
+      },
+      "descriptive": [
+        "dark",
+        "ashy",
+        "somber",
+        "bleak",
+        "muddy",
+        "sooty"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          [
+            0,
+            0.3
+          ]
+        ]
+      },
+      "descriptive": [
+        "dark",
+        "dim",
+        "gloomy",
+        "dull"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.1,
+            0.7
+          ],
+          [
+            0.15,
+            0.5
+          ]
+        ]
+      },
+      "descriptive": [
+        "bleak",
+        "muted",
+        "matte",
+        "dusty"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.12,
+            1
+          ],
+          [
+            0.7,
+            1
+          ]
+        ]
+      },
+      "descriptive": [
+        "tinted"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.2,
+            1
+          ],
+          [
+            0.75,
+            0.95
+          ]
+        ]
+      },
+      "descriptive": [
+        "pastel"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          [
+            0.88,
+            1
+          ]
+        ]
+      },
+      "descriptive": [
+        "pale",
+        "light",
+        "faded",
+        "delicate",
+        "glistening",
+        "bleached"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.5,
+            1
+          ],
+          [
+            0.7,
+            0.9
+          ]
+        ]
+      },
+      "descriptive": [
+        "fresh",
+        "sparkling",
+        "glittering",
+        "glowing",
+        "jazzy",
+        "opalecent"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          [
+            0.9,
+            1
+          ]
+        ]
+      },
+      "descriptive": [
+        "neutral"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.74,
+            1
+          ],
+          [
+            0.9,
+            1
+          ]
+        ]
+      },
+      "descriptive": [
+        "muted"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          1
+        ]
+      },
+      "descriptive": [
+        "colorless",
+        "bright",
+        "briliant",
+        "high"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          0
+        ]
+      },
+      "descriptive": [
+        "colorless",
+        "low",
+        "dark"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          [
+            0.01,
+            0.3
+          ]
+        ]
+      },
+      "descriptive": [
+        "shady"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          [
+            0.01,
+            0.08
+          ]
+        ]
+      },
+      "descriptive": [
+        "almost black"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          null,
+          [
+            0,
+            0.09
+          ]
+        ]
+      },
+      "descriptive": [
+        "very dark"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0,
+            0.4
+          ],
+          [
+            0,
+            0.22
+          ]
+        ]
+      },
+      "descriptive": [
+        "dark"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0,
+            0.04
+          ],
+          [
+            0.1,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "grey"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.04,
+            0.1
+          ],
+          [
+            0.17,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "almost grey"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.1,
+            0.3
+          ],
+          [
+            0.17,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "very unsaturated"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.3,
+            0.46
+          ],
+          null
+        ]
+      },
+      "descriptive": [
+        "unsaturated"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.46,
+            0.6
+          ],
+          [
+            0.4,
+            0.55
+          ]
+        ]
+      },
+      "descriptive": [
+        "rather unsaturated"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.601,
+            0.8
+          ],
+          [
+            0.45,
+            0.6
+          ]
+        ]
+      },
+      "descriptive": [
+        "saturated"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.801,
+            0.94
+          ],
+          [
+            0.45,
+            0.6
+          ]
+        ]
+      },
+      "descriptive": [
+        "rather saturated"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          null,
+          [
+            0.941,
+            1
+          ],
+          [
+            0.45,
+            0.6
+          ]
+        ]
+      },
+      "descriptive": [
+        "very saturated"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            0,
+            90
+          ],
+          [
+            0.3,
+            1
+          ],
+          [
+            0.1,
+            1
+          ]
+        ]
+      },
+      "descriptive": [
+        "warm",
+        "mellow"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            270,
+            360
+          ],
+          [
+            0.3,
+            1
+          ],
+          [
+            0.1,
+            1
+          ]
+        ]
+      },
+      "descriptive": [
+        "warm",
+        "mellow"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            90.01,
+            269.99
+          ],
+          null,
+          [
+            0.1,
+            1
+          ]
+        ]
+      },
+      "descriptive": [
+        "cold",
+        "cool"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            0,
+            15
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "red",
+        "reddish"
+      ],
+      "nouns": [
+        "red"
+      ],
+      "emotions": [
+        "excitement",
+        "energy",
+        "passion",
+        "courage",
+        "attention",
+        "lust",
+        "power",
+        "love",
+        "speed",
+        "anger",
+        "lust",
+        "excitement"
+      ],
+      "usage": [
+        "stimulate",
+        "createe urgency",
+        "draw attention",
+        "caution",
+        "encurage",
+        "excite"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            15,
+            45
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.4,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "orange"
+      ],
+      "nouns": [
+        "orange"
+      ],
+      "emotions": [
+        "optimism",
+        "idependence",
+        "adventure",
+        "creativity",
+        "fun"
+      ],
+      "usage": [
+        "stimulate",
+        "draw attention",
+        "express freedom",
+        "fascinate"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            15,
+            45
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.4
+          ]
+        ]
+      },
+      "descriptive": [
+        "brown"
+      ],
+      "nouns": [
+        "brown"
+      ],
+      "emotions": [
+        "strength",
+        "reliability",
+        "resilience",
+        "loneliness",
+        "sadness",
+        "isolation",
+        "warmth",
+        "comfort",
+        "security"
+      ],
+      "usage": [
+
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            45,
+            70
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "yellow"
+      ],
+      "nouns": [
+        "yellow"
+      ],
+      "emotions": [
+        "ethusiasm",
+        "opportunity",
+        "spontaneity",
+        "happiness",
+        "positivity"
+      ],
+      "usage": [
+        "stimulate",
+        "relax",
+        "awake awareness",
+        "egnergize",
+        "affect mood",
+        "sale",
+        "cheap",
+        "budget",
+        "construction",
+        "convey competence"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            70,
+            79
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "lime"
+      ],
+      "nouns": [
+        "lime"
+      ],
+      "emotions": [
+        "growth",
+        "harmony",
+        "fertility",
+        "kindness",
+        "dependability"
+      ],
+      "usage": [
+        "restore energy",
+        "promote growth",
+        "awake awareness",
+        "rejuvinate",
+        "nature"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            79,
+            163
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "green",
+        "greenish"
+      ],
+      "nouns": [
+        "green"
+      ],
+      "emotions": [
+        "safety",
+        "harmony",
+        "stability",
+        "reliability",
+        "balance"
+      ],
+      "usage": [
+        "relax",
+        "ballance",
+        "revitalize",
+        "encurage",
+        "posses"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            163,
+            193
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "cyan"
+      ],
+      "nouns": [
+        "cyan"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            193,
+            240
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "blue",
+        "blueish"
+      ],
+      "nouns": [
+        "blue"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            240,
+            260
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "indigo"
+      ],
+      "nouns": [
+        "indigo"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            260,
+            270
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "violet"
+      ],
+      "nouns": [
+        "violet"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            270,
+            291
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "purple",
+        "purplish"
+      ],
+      "nouns": [
+        "purple"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            291,
+            327
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "magenta"
+      ],
+      "nouns": [
+        "magenta"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            327,
+            344
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "pink"
+      ],
+      "nouns": [
+        "pink"
+      ]
+    },
+    {
+      "criteria": {
+        "hsl": [
+          [
+            344,
+            360
+          ],
+          [
+            0.1,
+            1
+          ],
+          [
+            0.07,
+            0.99
+          ]
+        ]
+      },
+      "descriptive": [
+        "red",
+        "reddish"
+      ],
+      "nouns": [
+        "red"
+      ],
+      "emotions": [
+        "excitement",
+        "energy",
+        "passion",
+        "courage",
+        "attention",
+        "lust",
+        "power",
+        "love",
+        "speed",
+        "anger"
+      ],
+      "usage": [
+        "stimulate",
+        "createe urgency",
+        "draw attention",
+        "caution",
+        "encurage"
+      ]
+    }
+  ],
+  "temperatures": [{
+      "value": 1800,
+      "descriptive": ["ultra warm"]
+    },
+    {
+      "value": 2400,
+      "descriptive": ["very warm"]
+    },
+    {
+      "value": 2700,
+      "descriptive": ["warm"]
+    },
+    {
+      "value": 3000,
+      "descriptive": ["warm white"]
+    },
+    {
+      "value": 4000,
+      "descriptive": ["cool or cold"]
+    },
+    {
+      "value": 6500,
+      "descriptive": ["cool"]
+    }
+  ],
+  "percentWords": [{
+      "maxPercentile": 0.06,
+      "word": "a dash of"
+    },
+    {
+      "maxPercentile": 0.16,
+      "word": "a little bit of"
+    },
+    {
+      "maxPercentile": 0.31,
+      "word": "some"
+    },
+    {
+      "maxPercentile": 0.56,
+      "word": "a good bit of"
+    },
+    {
+      "maxPercentile": 0.71,
+      "word": "a lot of"
+    },
+    {
+      "maxPercentile": 0.86,
+      "word": "a whole lot of"
+    },
+    {
+      "maxPercentile": 0.99,
+      "word": "neatly entirely"
+    },
+    {
+      "maxPercentile": 1,
+      "word": "entirely"
+    }
+  ]
+}
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import ColorDescription from './src/index';
+import wordsEN from './src/en';
 
 test('parses color corectly', () => {
   const cd = new ColorDescription('red');
@@ -6,19 +7,6 @@ test('parses color corectly', () => {
   expect(cd).toHaveProperty('color');
   expect(cd.color.hex()).toBe('#ff0000');
 });
-
-/* find a way to test that */
-/*
-test('does not like funky colors', () => {
-  const cd = new ColorDescription('redrum');
-
-  const t = () => {
-    throw new TypeError();
-  };
-
-  expect(cd).toThrow(TypeError);
-});
-*/
 
 test('get adjectives for color temperature', () => {
   const cd = new ColorDescription('red');
